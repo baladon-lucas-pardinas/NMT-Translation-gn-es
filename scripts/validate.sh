17a3fc408d6bf6bfd869c318112a459a5967f5b1
alexis.baladon <alexis.baladon@fing.edu.uy>
2023-05-31 11:10:54 -0300

    [FEATURE] Moses scripts used in validation

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-./postprocess.sh < $1 > file.out 2>/dev/null
-./moses-scripts/scripts/generic/multi-bleu-detok.perl file.ref < file.out 2>/dev/null \
+
+SCRIPT_DIR=$(dirname "$0")
+
+cat $1 \
+    | sed 's/\@\@ //g' \
+    | ./moses-scripts/scripts/recaser/detruecase.perl 2>/dev/null \
+    | ./moses-scripts/scripts/tokenizer/detokenizer.perl -l es 2>/dev/null \
+    | ./moses-scripts/scripts/generic/multi-bleu-detok.perl ${SCRIPT_DIR}/../artifacts/data/validation/valid_es.txt.es \
     | sed -r 's/BLEU = ([0-9.]+),.*/\1/
