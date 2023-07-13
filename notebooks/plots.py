afc5f2c31c275d0794ef32eefd6ae08e741e40d7
alexis.baladon <alexis.baladon@fing.edu.uy>
2023-07-12 22:30:19 -0300

    [DOCUMENTATION] Time analysis by hardware configuration

--- notebooks/plots.py
+++ notebooks/plots.py
@@ -6,6 +6,7 @@ SCORE_TYPE_COL = 'score_type'
 SCORE_COL = 'score'
 EPOCH_COL = 'epoch'
 MODEL_ID = 'model_name'
+DURATION_COL = 'duration'
 
 def plot_metrics_by_epoch(dataframe, metrics, save_path=None):
     num_metrics = len(metrics)
@@ -40,19 +41,32 @@ def create_df_from_results_csv(results_csv_path):
     df = pd.read_csv(results_csv_path)
     return df
 
-def plot_max_score_by_model(df: pd.DataFrame, metric, save_path=None):
+def plot_max_score_by_model(df: pd.DataFrame, metric, figsize=(20, 6), save_path=None):
     df = df[df[SCORE_TYPE_COL] == metric]
-    #maximum score of each model_id
     df = df.groupby(MODEL_ID)[SCORE_COL].max().reset_index()
     df = df.sort_values(by=SCORE_COL, ascending=False)
     sns.set(style='darkgrid')
-    plt.figure(figsize=(8, 6))
+    plt.figure(figsize=figsize)
     ax = sns.barplot(x=MODEL_ID, y=SCORE_COL, data=df)
 
     for i in ax.containers:
         ax.bar_label(i,)
 
     plt.title(metric)
+    plt.tight_layout()
+    plt.xticks(rotation=45)
+    plt.savefig(save_path) if save_path is not None else plt.show()
+
+def plot_time_by_model(df: pd.DataFrame, title: str, duration_col=DURATION_COL, figsize=(20, 6), save_path=None):
+    df = df.sort_values(by=duration_col, ascending=False)
+    sns.set(style='darkgrid')
+    plt.figure(figsize=figsize)
+    ax = sns.barplot(x=MODEL_ID, y=duration_col, data=df)
+
+    for i in ax.containers:
+        ax.bar_label(i,)
+
+    plt.title(title)
     plt.tight_layout()
     plt.xticks(rotation=45)
     plt.savefig(save_path) if save_path is not None else plt.show()
