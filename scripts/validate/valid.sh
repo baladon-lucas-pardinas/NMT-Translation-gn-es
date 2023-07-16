0fc534aa183a8ad68adbd22fce06d7e98290b779
alexis.baladon <alexis.baladon@fing.edu.uy>
2023-07-16 09:39:55 -0300

    [FIX] Marian appends output file to validation script as the last (not first) argument

--- scripts/validate/valid.sh
+++ scripts/validate/valid.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-DECODED_OUTPUT_FILE=$1
-PROJECT_DIR=$2
-REFERENCE_FILE=$3
-SCORE_TYPE=$4
+PROJECT_DIR=$1
+REFERENCE_FILE=$2
+SCORE_TYPE=$3
+DECODED_OUTPUT_FILE=$4
 SCORE_SCRIPT_DIR=${PROJECT_DIR}/scripts/validate
 python3 ${SCORE_SCRIPT_DIR}/score.py --reference_file ${REFERENCE_FILE} \
                                      --translation_file ${DECODED_OUTPUT_FILE} \
