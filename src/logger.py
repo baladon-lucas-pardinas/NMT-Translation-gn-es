8402acaa4c0b4dc28f5011213597ae7df8e1355d
alexis.baladon <alexis.baladon@fing.edu.uy>
2023-08-05 13:17:35 -0300

    [FIX] logger not working when project called from different directory

--- src/logger.py
+++ src/logger.py
@@ -2,9 +2,9 @@ import logging
 import os
 from datetime import datetime
 
-CURRENT_DIR = os.getcwd()
+PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 LOG_FILE = '{}.log'.format(datetime.now().strftime("%Y%m%d_%H%M%S"))
-LOG_PATH = os.path.join(CURRENT_DIR, 'logs', 'project')
+LOG_PATH = os.path.join(PROJECT_DIR, 'logs', 'project')
 
 if not os.path.exists(os.path.dirname(LOG_PATH)):
     os.makedirs(os.path.dirname(LOG_PATH))
